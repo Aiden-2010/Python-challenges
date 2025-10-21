3dc96c1cc06d2fe7aae56c26be75b3d7e3c70ba7
Aiden-2010 <aidenmartin.mgs@gmail.com>
2025-10-21 19:19:28 +0100

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,14 @@
+num1 = 4
+num2 = 5
+num3 = 8
+num4 = 2
+num5 = 0
+num5 = (num1 + num2)*num3/num4
+print("The value of num5",num5)
+
+name = "Alex"
+age = 0
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello, welcome")
+else:
+    print("goodbye")
